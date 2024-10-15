617b91cf1686edefde825097ff88e8b75b45dccf
david-koschel <113340344+david-koschel@users.noreply.github.com>
2024-10-15 23:33:45 +0100

    Adjusted contour finding and simplified coin detection formula

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,17 +1,10 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 img = cv2.imread('moneditas.JPEG')
 
-def show_image(img): #remove
-    plt.axis('off')
-    plt.imshow(img, cmap='gray')
-    plt.show()
-
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-pimg = cv2.medianBlur(gris, 7)
-show_image(pimg) #remove
+pimg = cv2.GaussianBlur(gris, (3, 3), 0)
 
 contornos2, hierarchy2 = cv2.findContours(pimg,
     cv2.RETR_EXTERNAL ,
@@ -21,10 +14,10 @@ circ = cv2.HoughCircles(
     pimg,
     cv2.HOUGH_GRADIENT,
     1,
-    40,
-    param1=180,
-    param2=35,
-    minRadius=70,
+    150,
+    param1=145,
+    param2=65,
+    minRadius=50,
     maxRadius=120
 )
 
@@ -52,7 +45,7 @@ def click_event(event, x, y, a, b):
                 return
 
 def count_money(euro1radius):
-    correlation = euro1radius / monedas[1]
+    correlation = monedas[1] / euro1radius
     result = {
         .01: 0,
         .02: 0,
@@ -65,7 +58,7 @@ def count_money(euro1radius):
     }
     for c in circ[0]:
         radius = c[2]
-        moneda = min(monedas, key=lambda x:abs(1 - (correlation * monedas[x] / radius)))
+        moneda = min(monedas, key=lambda x:abs(monedas[x] - (correlation * radius)))
         result[moneda] += 1
 
     count_result(result)
@@ -83,7 +76,7 @@ def draw_and_show(nimg):
         x_coor, y_coor, det_radio = det
         cv2.circle(nimg, (int(x_coor), int(y_coor)),
                    int(det_radio), (0, 255, 0), 2)
-    cv2.imshow('Monedas', cv2.resize(nimg, tuple(int(ti / 2.5) for ti in img.shape[1::-1])))
+    cv2.imshow('Monedas', cv2.resize(nimg, (0, 0), fx=0.4, fy=0.4))
     cv2.setMouseCallback('Monedas', click_event)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
